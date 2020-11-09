2b392075b83b60d64380da8a2206ce5d679b1e93
BangYongHyuk <kuk07k@naver.com>
2020-11-09 10:05:13 +0900

    20201109 수정

--- DataPrint_20201105/DataPrint_20201105/commons.cs
+++ DataPrint_20201105/DataPrint_20201105/commons.cs
@@ -9,7 +9,7 @@ namespace DataPrint_20201105
     class commons
     {
         //공용 연결 문자열
-        public static string CONNSTRING = "Data Source=192.168.1.110;Initial Catalog=DataDB;Persist Security Info=True;User ID=sa;Password=p@ssw0rd!!";
+        public static string CONNSTRING = "Data Source=172.30.1.31;Initial Catalog=DataDB;Persist Security Info=True;User ID=sa;Password=p@ssw0rd!!";
 
         public static string LOGINUSERID = "";
     }
